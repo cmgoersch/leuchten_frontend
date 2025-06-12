990b902c25acec3de6eaef7306c7bd3d5d252a79
Simon Kuhball <simon@kuhball.de>
2025-06-12 20:12:34 +0200

    fix: more sed fixes

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i -e "s/http:\/\/localhost:3000/$BACKEND_URL/g" /usr/share/nginx/html/script.js
+sed -i -e "s,http://localhost:3000,$BACKEND_URL,g" /usr/share/nginx/html/script.js
 nginx -g "daemon off;"
